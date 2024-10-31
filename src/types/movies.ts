dece492061984f9d3a27071975e9fc4004a52e87
prrodriguess <pr.rodriguessrp@gmail.com>
2024-10-31 11:21:24 -0300

    setup react and define types for movie

---
+++ src/types/movies.ts
@@ -0,0 +1,8 @@
+// Here is what the movie should contain as movie
+export interface Movie {
+    id: number;
+    title: string;
+    releaseYear: number;
+    genre: string;
+    description: string;
+}
